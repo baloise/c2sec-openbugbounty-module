5e62be9606bfae2328d2f6076e0e7b654657ef26
renat1sakenov <renat.sakenov@googlemail.com>
2018-06-28 12:42:01 +0200

    separate database handler

---
+++ database_handler.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * OPENBUGBOUNTY C2SEC MODULE
+ *
+ *
+ */
+namespace obb;
+
+
+/**
+ * Database handling class
+ */
+class DatabaseHandler{
+
+    /**
+     * Database connection.
+     * Only DomainData Object are saved in the database.
+     * This data will only be used if information about ALL domains is requested. 
+     * For a regular request, regarding one domain, the information will be retrieved from the API.
+     */
+    private $conn;
+
+    public function __construct($server,$user,$pass,$db){
+    
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+        $this->conn = new \mysqli($server,$user,$pass,$db);
+
+        /*
+            create tables in first run
+            For now I will simply encode all reports and types into a string, so there won't be a need for additional tables and queries.
+            Later this might change, if there will be a need to query for a single vulnerablitity or similiar.
+        */
+        $res = $this->conn->query("CREATE TABLE IF NOT EXISTS domain_data 
+                                    (host VARCHAR(50),
+                                    reports LONGTEXT,
+                                    total INT, 
+                                    fixed INT, 
+                                    time BIGINT, 
+                                    average_time DOUBLE, 
+                                    percentage_fixed FLOAT, 
+                                    types TEXT,
+                                    PRIMARY KEY(host))");
+    }    
+
+    /**
+     * Loads all data from the database into an array of DomainData
+     * @return DomainData[]
+     */
+    public function load_domain_data(){
+
+        $domain_list = array();
+        $res = $this->conn->query("SELECT * FROM domain_data");
+        while(($row = $res->fetch_assoc())){
+            $host = $row['host'];
+            $domain_list[$host] = new DomainData($host);
+            $domain_list[$host]->reports = json_decode($row['reports']);
+            $domain_list[$host]->total = $row['total'];
+            $domain_list[$host]->fixed = $row['fixed'];
+            $domain_list[$host]->time = $row['time'];
+            $domain_list[$host]->average_time = $row['average_time'];
+            $domain_list[$host]->percentage_fixed = $row['percentage_fixed'];
+            $domain_list[$host]->types = json_decode($row['types']);
+            $domain_list[$host]->to_update = false;
+        }
+        return $domain_list;
+    }
+
+    /**
+     * Write a DomainData object to the database or update it
+     * @param DomainData 
+     */
+    public function write_database($domain_data){
+
+        $stmt = $this->conn->prepare("REPLACE INTO domain_data (host,reports,total,fixed,time,average_time,percentage_fixed,types) VALUES (?,?,?,?,?,?,?,?)");
+        $stmt->bind_param("ssiiidds",$domain_data->host,
+                                    json_encode($domain_data->reports),
+                                    $domain_data->total,
+                                    $domain_data->fixed,
+                                    $domain_data->time,
+                                    $domain_data->average_time,
+                                    $domain_data->percentage_fixed,
+                                    json_encode($domain_data->types));
+        $res = $stmt->execute();
+        $stmt->close();
+        if(!$res){
+            #log
+            echo "database write could not be performed";
+        }
+    }
+
+    /**
+     * Writes a bulk (of save_bulk_size) into the database
+     * @param DomainData[] 
+     * @return DomainData[] the updated list
+     */
+    public function write_bulk($domain_list){
+        foreach($domain_list as $domain_data){
+            if($domain_data->to_update){
+                $domain_data->sumUp();
+                $this->write_database($domain_data);
+            }
+        }
+        return $domain_list;
+    }
+}
+?>
--- obb.php
+++ obb.php
@@ -8,6 +8,7 @@ namespace obb;
 
 require_once 'functions.php';
 require_once 'domain_data.php';
+require_once 'database_handler.php';
 
 /**
  * Mainclass of this module.
@@ -36,18 +37,16 @@ class Obb {
      */
     private $save_bulk_size = 50;
 
-    /**
-     * Database connection.
-     * Only DomainData Object are saved in the database.
-     * This data will only be used if information about ALL domains is requested. 
-     * For a regular request, regarding one domain, the information will be retrieved from the API.
-     */
-    private $conn;
 
     /**
      * path to file containing all incident ids that need checking (if the incident got fixed)
      */
     private $to_update_file;
+
+    /**
+     * 
+     */
+    private $database_handler;
 
     /**
      * The constructor of an obb instance. 
@@ -70,30 +69,13 @@ class Obb {
             $this->to_update_file = './.to_update_file';
         }
 
-        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-
         $server = $config["db_server"];
         $user = $config["db_user"]; 
         $pass = $config["db_pass"];
         $db = $config["database"];
         
-        $this->conn = new \mysqli($server,$user,$pass,$db);
-
-        /*
-            create tables in first run
-            For now I will simply encode all reports and types into a string, so there won't be a need for additional tables and queries.
-            Later this might change, if there will be a need to query for a single vulnerablitity or similiar.
-        */
-        $res = $this->conn->query("CREATE TABLE IF NOT EXISTS domain_data 
-                                    (host VARCHAR(50),
-                                    reports LONGTEXT,
-                                    total INT, 
-                                    fixed INT, 
-                                    time BIGINT, 
-                                    average_time DOUBLE, 
-                                    percentage_fixed FLOAT, 
-                                    types TEXT,
-                                    PRIMARY KEY(host))");
+        $this->database_handler = new DatabaseHandler($server,$user,$pass,$db);       
+ 
     }
 
     #testing
@@ -211,30 +193,6 @@ class Obb {
 
 
     /**
-     * Loads all data from the database into an array of DomainData
-     * @return DomainData[]
-     */
-    private function load_domain_data(){
-
-        $domain_list = array();
-        $res = $this->conn->query("SELECT * FROM domain_data");
-        while(($row = $res->fetch_assoc())){
-            $host = $row['host'];
-            $domain_list[$host] = new DomainData($host);
-            $domain_list[$host]->reports = json_decode($row['reports']);
-            $domain_list[$host]->total = $row['total'];
-            $domain_list[$host]->fixed = $row['fixed'];
-            $domain_list[$host]->time = $row['time'];
-            $domain_list[$host]->average_time = $row['average_time'];
-            $domain_list[$host]->percentage_fixed = $row['percentage_fixed'];
-            $domain_list[$host]->types = json_decode($row['types']);
-            $domain_list[$host]->to_update = false;
-        }
-        return $domain_list;
-    }
-
-
-    /**
      * Returns a list von DomainData Objects, each for a different domain.
      * Iterating through all incidents
      * THIS MIGHT TAKE A LONG TIME AND/OR MAYBE OPENBUGBOUNTY WILL CLOSE THE CONNECTION DUE TO TOO MANY REQUESTS.
@@ -243,7 +201,7 @@ class Obb {
     public function get_all_domains(){
     
         #load all domaindata object from database
-        $domain_list = $this->load_domain_data();
+        $domain_list = $this->database_handler->load_domain_data();
         #keeping track of when we need to save the data to the drive
         $bulk_counter = 0;
         
@@ -254,8 +212,18 @@ class Obb {
         if($update_file_handle){
             while(($line = fgets($update_file_handle))){
                 $id = (int)$line;
-                $res = $this->get_response($this->id_url . $id);
+                try{
+                    $res = $this->get_response($this->id_url . $id);
+                }catch(NoResultException $e){
+                    continue;
+                }
                 $host = (string)$res->children()[0]->host;
+                #It is possible that we have marked some domains for update, even though we have not saved them
+                #(when we terminate the run between bulk writes) 
+                #so we ignore them just for now, they will be written back later
+                if(NULL == $domain_list[$host]){
+                    continue;
+                }
                 if(1 == $res->children()[0]->fixed){
                     $domain_list[$host]->add($res->children()[0]);
                 }else{
@@ -291,13 +259,25 @@ class Obb {
                fwrite($update_file_handle,$counter . "\n"); 
             }
             if($bulk_counter >= $this->save_bulk_size){
-                $domain_list = $this->write_bulk($domain_list);
+                $domain_list = $this->database_handler->write_bulk($domain_list);
+                $this->update_incident_index($counter);
                 $bulk_counter = 0;
             }
         }
         fclose($update_file_handle);
-        #write to init file
-        #unpleasent solution, may require redo or import of pear Config_Lite
+
+        #sum everyone up, with to_update flag, and write to database
+        $this->update_incident_index($latest_id);
+        $domain_list = $this->database_handler->write_bulk($domain_list);
+        return $domain_list;
+    }
+
+
+    /**
+     * write to incident_index init file
+     * unpleasent solution, may require redo or import of pear Config_Lite
+     */
+    private function update_incident_index($latest_id){
         $ini_handler = fopen(CONFIG,'r');
         $ini_handler_new = fopen(CONFIG . "~",'w+');
         if($ini_handler){
@@ -313,50 +293,6 @@ class Obb {
             unlink(CONFIG);
             rename(CONFIG . "~", CONFIG);
         }
-
-        #sum everyone up, with to_update flag, and write to database
-        $domain_list = $this->write_($domain_list);
-        return $domain_list;
-    }
-
-
-    
-    /**
-     * Write a DomainData object to the database or update it
-     * @param DomainData 
-     */
-    private function write_database($domain_data){
-
-        $stmt = $this->conn->prepare("REPLACE INTO domain_data (host,reports,total,fixed,time,average_time,percentage_fixed,types) VALUES (?,?,?,?,?,?,?,?)");
-        $stmt->bind_param("ssiiidds",$domain_data->host,
-                                    json_encode($domain_data->reports),
-                                    $domain_data->total,
-                                    $domain_data->fixed,
-                                    $domain_data->time,
-                                    $domain_data->average_time,
-                                    $domain_data->percentage_fixed,
-                                    json_encode($domain_data->types));
-        $res = $stmt->execute();
-        $stmt->close();
-        if(!$res){
-            #log
-            echo "database write could not be performed";
-        }
-    }
-
-    /**
-     * Writes a bulk (of save_bulk_size) into the database
-     * @param DomainData[] 
-     * @return DomainData[] the updated list
-     */
-    private function write_bulk($domain_list){
-        foreach($domain_list as $domain_data){
-            if($domain_data->to_update){
-                $domain_data->sumUp();
-                $this->write_database($domain_data);
-            }
-        }
-        return $domain_list;
     }
 
     /**
